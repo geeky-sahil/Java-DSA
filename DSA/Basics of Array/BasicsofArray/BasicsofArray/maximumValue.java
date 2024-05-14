dd9b856ca4ac0cfd5f36a901883d4af654caa976
geekysahil-lab <sahilprasad05@icloud.com>
2024-05-14 12:01:56 +0530

    Updated

--- DSA/Basics of Array/BasicsofArray/BasicsofArray/maximumValue.java
+++ DSA/Basics of Array/BasicsofArray/BasicsofArray/maximumValue.java
@@ -11,8 +11,12 @@ public class maximumValue {
         for (int i = 0; i < arr.length; i++) {
             arr[i]=sc.nextInt();
         }
-        int mx=arr[0];
+        // int mx=arr[0];
+        int mx=Integer.MIN_VALUE;//smallest integer value
         for (int i = 0; i < arr.length; i++) {
+
+            //mx=Math.max(mx,arr[i]); 
+            //remove if block
            if(arr[i]>mx) 
            {
             mx=arr[i];
---
+++ DSA/Basics of Array/BasicsofArray/BasicsofArray/secondLargest.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class secondLargest {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        int n=sc.nextInt();
+        System.out.println("Enter the elements one by one:");
+        int []arr=new int[n];
+        n=arr.length;
+        for (int i = 0; i < arr.length; i++) {
+           arr[i]=sc.nextInt(); 
+        }
+        int mx=arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>mx)
+            {
+                mx=arr[i];
+            }
+        }
+        
+        int smx;
+        smx=arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]!=mx)
+            {
+                smx=Math.max(smx, arr[i]);
+            }
+        }
+        System.out.println("Second Largest Element is:"+smx);
+    }
+    
+}
